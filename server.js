41e832bf7f3682ac3c4f0e5dab1bdd862a282f84
Vitor Gomes <vct@outlook.com>
2026-05-13 16:44:47 +0000

    Analytics do Firebase

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.post('/push', limitPush, async (req, res) => {
         data,
         android: {
             priority: 'high',
+            fcmOptions: { analyticsLabel: tipo },
             notification: {
                 channelId: isVisivel ? 'forca_vendas_v2' : 'fv_sync_silent',
                 tag: tipo,
